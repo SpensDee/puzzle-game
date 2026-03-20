86b81f0221fd280d27f0060584b7050072ca9ae2
Ilyas Bezhanidze <spensdee@gmail.com>
2026-03-20 12:01:01 +0200

    test

--- sw.js
+++ sw.js
@@ -1,7 +1,26 @@
 const CACHE_NAME = 'puzzle-v2';
 
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', e => e.waitUntil(clients.claim()));
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting(); // ← не ждёт закрытия вкладок
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    // Удаляем ВСЕ старые кеши
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    ).then(() => clients.claim()) // ← сразу берёт контроль
+  );
+});
 
 self.addEventListener('fetch', e => {
   const url = e.request.url;
@@ -9,8 +28,7 @@ self.addEventListener('fetch', e => {
     e.respondWith(
       caches.match(e.request).then(r => r || fetch(e.request).then(resp => {
         if (resp.ok) {
-          const c = resp.clone();
-          caches.open(CACHE_NAME).then(cache => cache.put(e.request, c));
+          caches.open(CACHE_NAME).then(cache => cache.put(e.request, resp.clone()));
         }
         return resp;
       }))
