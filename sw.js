b77e290c3e869aa0b0a40e7e1525d1f7d3f51738
Ilyas Bezhanidze <spensdee@gmail.com>
2026-03-20 11:59:17 +0200

    ccategories

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'puzzle-v1';
+const CACHE_NAME = 'puzzle-v2';
 
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => e.waitUntil(clients.claim()));
